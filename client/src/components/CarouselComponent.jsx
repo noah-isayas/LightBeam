fa58aace3d5781032adc6931ab1e86c5df1717aa
Michael Nikolaisen <michaelhalnik@gmail.com>
2024-06-07 03:55:31 +0200

    added watchscreen2

--- client/src/components/CarouselComponent.jsx
+++ client/src/components/CarouselComponent.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import WatchScreen from "./WatchScreen.jsx";
-import ImageTemplate from "./ImageTemplate.jsx"; // Make sure the path is correct
+import ImageTemplate from "./ImageTemplate.jsx";
+import WatchScreen2 from "./WatchScreen2.jsx"; // Make sure the path is correct
 
 function CarouselComponent() {
     return (
